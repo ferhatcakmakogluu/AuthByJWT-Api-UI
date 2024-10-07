ec18ec0e2210ecb9eb8f7dd4cbca4c39eb06f504
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-07 16:38:19 +0300

    Core layer completed

---
+++ AuthByJWT.Core/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthByJWT.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetById(string id);
+        Task AddAsync(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
